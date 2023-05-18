c240ae4e2639b63e75baebfc1d6910337295b86a
Arya TS <arya.ts@livares.com>
2023-05-18 17:22:55 +0530

    API to update an existing user(PUT)

--- UserIdentity/views.py
+++ UserIdentity/views.py
@@ -37,11 +37,17 @@ class userDetails(APIView):
             return User.objects.get(pk=pk)
         except:
             raise Http404
-            
-             
-         
+           
     def get(self,request,pk,format=None):
         userData=self.get_object(pk)
         serializer=UserDataSerializer(userData)
-        return Response(serializer.data) 
+        return Response(serializer.data)
+
+    def put(self,request,pk,format=None):
+        userData=self.get_object(pk)
+        serializer=UserDataSerializer(userData,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({"message":'error','error':serializer.errors}) 
     
