fd1c16d4f9159ac88e8628264c888c9a5b8b1a94
Arya TS <arya.ts@livares.com>
2023-05-30 12:32:24 +0530

    Revert "Revert "API for IMAGE file (JPEG/PNG) upload""
    This reverts commit 483d6c289490f60a111abdfccc38afb177e4d27f.

--- FileStorage/views.py
+++ FileStorage/views.py
@@ -6,10 +6,17 @@ from .serializers import UploadSerializer
 # Create your views here.
 class UploadAPIView(APIView):
     def post(self, request, format=None):
+        allowed_types = ['image/jpeg', 'image/png']
         serializer = UploadSerializer(data=request.data)
+
         if serializer.is_valid():
+            if serializer.validated_data['file'].content_type not in allowed_types:
+                return Response("Only JPG and PNG files are allowed.", status=400)
+
             serializer.save()
-            
-            serialized_data = serializer.data
-            return Response(serialized_data)
-        return Response(serializer.errors)
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=400)
+
+
+
