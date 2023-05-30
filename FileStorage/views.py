41c81ad8342ca3230b0c9f48ec088aba4686ca9c
Arya TS <arya.ts@livares.com>
2023-05-30 12:29:13 +0530

    API for IMAGE file (JPEG/PNG) upload

--- FileStorage/views.py
+++ FileStorage/views.py
@@ -4,12 +4,36 @@ from rest_framework.response import Response
 from .serializers import UploadSerializer
 
 # Create your views here.
+# class UploadAPIView(APIView):
+#     def post(self, request, format=None):
+#         allowed_types = ['image/jpeg', 'image/png']
+#         serializer = UploadSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             if serializer not in allowed_types:
+#                 return Response("Only JPG and PNG files are allowed.", status=400)
+          
+
+#             serializer.save()
+#             serialized_data = serializer.data
+#             return Response(serialized_data)
+#         return Response(serializer.errors)
+
+
+
 class UploadAPIView(APIView):
     def post(self, request, format=None):
+        allowed_types = ['image/jpeg', 'image/png']
         serializer = UploadSerializer(data=request.data)
+
         if serializer.is_valid():
+            if serializer.validated_data['file'].content_type not in allowed_types:
+                return Response("Only JPG and PNG files are allowed.", status=400)
+
             serializer.save()
-            
-            serialized_data = serializer.data
-            return Response(serialized_data)
-        return Response(serializer.errors)
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=400)
+
+
+
